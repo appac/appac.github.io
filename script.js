21991c3234f5f14f650e8189f7045e9e65b90969
Chris Apparicio <chris@apparicio.me>
2017-06-24 22:54:41 +0100

    re-design to use jsRender

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+	'use strict';
+	loadProjects();
+
+	
+
+	$(".card").on("mouseenter", function () {
+		$(".card-footer").show();
+	});
+
+	function loadProjects() {
+		$.ajax({
+			url: 'projects.json',
+			success: function (projects) {
+				projects.forEach(function (project) {
+					renderProject(project);
+				});
+			},
+			error: function (error) {
+				console.log(error);
+			}
+		})
+	}
+
+	function renderProject(project) {
+		var template = $.templates("#project");
+		var html = template.render(project);
+		$("#projects").append(html);
+	}
+});

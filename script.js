d5c6772a335c9050a0c4b23e513b393668ff02dc
Chris Apparicio <chris@apparicio.me>
2017-06-25 23:45:11 +0100

    tidy

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
 $(document).ready(function () {
 	'use strict';
 	loadProjects();
-
-	
-
-	$(".card").on("mouseenter", function () {
-		$(".card-footer").show();
-	});
-
-	function loadProjects() {
-		$.ajax({
-			url: 'projects.json',
-			success: function (projects) {
-				projects.forEach(function (project) {
-					renderProject(project);
-				});
-			},
-			error: function (error) {
-				console.log(error);
-			}
-		});
-	}
-
-	function renderProject(project) {
-		var template = $.templates("#project");
-		var html = template.render(project);
-		$("#projects").append(html);
-	}
 });
+
+function loadProjects() {
+	$.ajax({
+		url: 'projects.json',
+		success: function (projects) {
+			projects.forEach(function (project) {
+				renderProject(project);
+			});
+		},
+		error: function (error) {
+			console.log(error);
+		}
+	});
+}
+
+function renderProject(project) {
+	var template = $.templates("#project");
+	var html = template.render(project);
+	$("#projects").append(html);
+}
